64df1d509f542cfdc3b5f851282c1330ae79c0fc
Alexander <renyreny125tt@gmail.com>
2022-06-03 15:09:56 +0300

    feat: update function read lines of file

--- src/app.js
+++ src/app.js
@@ -1,53 +1,88 @@
-const fs = require('fs');
+const fs = require("mz/fs");
 
-// Примерное число байт в строке
-const numberLetterLines = 1000;
+async function readLastLines (input_file_path, maxLineCount, encoding) {
 
-async function readLinesFile(fileName, numberLines) {
-  const start = Date.now();
-  let sizeFile = 0;
-  const bufferSize = numberLines * numberLetterLines;
-  
-  fs.stat(('files/' + fileName), (error, stats) => {
-      if (error) {
-        console.error(error);
-      }
-      else {
-        sizeFile = stats.size;
-      }
-  });
+  const NEW_LINE_CHARACTERS = ["\n"];
 
-  fs.open('files/' + fileName, 'r', function (err, fd) {
-    if (err) {
-      console.error(err);
-    }
-    
-    const buf = new Buffer.allocUnsafe(sizeFile > bufferSize ? bufferSize : sizeFile); 
+  if (encoding == null) {
+    encoding = "utf8";
+  }
 
-    fs.read(fd, buf, 0, buf.length, sizeFile > bufferSize ? sizeFile - bufferSize : 0, function (err, bytes) {  
-      if (err) {
-        console.error(err);
-      }
-      
-      if (bytes > 0) {
-        const resBufLines = buf.slice(0).toString().split('\r');
-        const resLines = resBufLines.slice(-numberLines).join(' ');      
-        console.log(resLines);
-        console.log('\nКоличество строк ' + resBufLines.slice(-numberLines).length);
-        const end = Date.now();
-        console.log((end - start) / 1000, 's');
-      }
-
-      fs.close(fd, function (err) {
-        if (err) {
-          console.error(err);
-        }
+  const readPreviousChar = function( stat, file, currentCharacterCount) {
+    return fs.read(file, Buffer.alloc(1), 0, 1, stat.size - 1 - currentCharacterCount)
+      .then((bytesReadAndBuffer) => {
+        return String.fromCharCode(bytesReadAndBuffer[1][0]);
       });
-    });
+  };
+
+  return new Promise((resolve, reject) => {
+    let self = {
+      stat: null,
+      file: null,
+    };
+
+    fs.exists(input_file_path)
+      .then((exists) => {
+        if (!exists) {
+          throw new Error("file does not exist");
+        }
+
+      }).then(() => {
+        let promises = [];
+
+        promises.push(
+          fs.stat(input_file_path)
+            .then(stat => self.stat = stat));
+
+        promises.push(
+          fs.open(input_file_path, "r")
+            .then(file => self.file = file));
+
+        return Promise.all(promises);
+      }).then(() => {
+        let chars = 0;
+        let lineCount = 0;
+        let lines = "";
+
+        const do_while_loop = function() {
+          if (lines.length > self.stat.size) {
+            lines = lines.substring(lines.length - self.stat.size);
+          }
+
+          if (lines.length >= self.stat.size || lineCount >= maxLineCount) {
+            if (NEW_LINE_CHARACTERS.includes(lines.substring(0, 1))) {
+              lines = lines.substring(1);
+            }
+            fs.close(self.file);
+            if (encoding === "buffer") {
+              return resolve(Buffer.from(lines, "binary"));
+            }
+            return resolve(Buffer.from(lines, "binary").toString(encoding));
+          }
+
+          return readPreviousChar(self.stat, self.file, chars)
+            .then((nextCharacter) => {
+              lines = nextCharacter + lines;
+              if (NEW_LINE_CHARACTERS.includes(nextCharacter) && lines.length > 1) {
+                lineCount++;
+              }
+              chars++;
+            })
+            .then(do_while_loop);
+        };
+        return do_while_loop();
+
+      }).catch((reason) => {
+        if (self.file !== null) {
+          fs.close(self.file).catch(() => {
+          });
+        }
+        return reject(reason);
+      });
   });
 }
 
 const fileNameArr = ['textEn.txt', 'textRu.txt', 'textSmall.txt'];
 const numberLines = 5;
 
-readLinesFile(fileNameArr[0], numberLines);
+readLastLines(__dirname + '/../files/' + fileNameArr[0], numberLines).then((lines) => console.log(lines));
